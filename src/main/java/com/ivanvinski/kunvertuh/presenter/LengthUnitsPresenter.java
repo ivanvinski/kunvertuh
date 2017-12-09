782d21c7c6a960d409a5df85140eada4b291431b
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-09 15:59:50 +0100

    Fix LengthUnitsPresenter bug
    View was not updated on conversion. There shall be more unit tests.

--- src/main/java/com/ivanvinski/kunvertuh/presenter/LengthUnitsPresenter.java
+++ src/main/java/com/ivanvinski/kunvertuh/presenter/LengthUnitsPresenter.java
@@ -12,12 +12,15 @@ import static javax.measure.unit.SI.MILLIMETER;
 import com.ivanvinski.kunvertuh.model.LengthUnitsModel;
 import com.ivanvinski.kunvertuh.view.LengthUnitsView;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.Objects;
 
 public class LengthUnitsPresenter {
 
   private LengthUnitsView view;
   private LengthUnitsModel model;
+  private DecimalFormat decimalFormat = new DecimalFormat("#0.##########");
 
   public LengthUnitsPresenter(LengthUnitsView view, LengthUnitsModel model) {
     Objects.requireNonNull(view, "LengthUnitsView can't be null");
@@ -25,6 +28,23 @@ public class LengthUnitsPresenter {
     this.view = view;
     this.model = model;
     bindEventsToImplementation();
+  }
+
+  public void convert(String sourceLength, Object sourceUnit) {
+    BigDecimal conversionValue = toBigDecimal(sourceLength);
+    model.convert(conversionValue, sourceUnit);
+    view.setMillimeters(formatBigDecimal(model.getMillimeters()));
+    view.setCentimeters(formatBigDecimal(model.getCentimeters()));
+    view.setMeters(formatBigDecimal(model.getMeters()));
+    view.setKilometers(formatBigDecimal(model.getKilometers()));
+    view.setInches(formatBigDecimal(model.getInches()));
+    view.setFeet(formatBigDecimal(model.getFeet()));
+    view.setYards(formatBigDecimal(model.getYards()));
+    view.setMiles(formatBigDecimal(model.getMiles()));
+  }
+
+  public DecimalFormat getDecimalFormat() {
+    return decimalFormat;
   }
 
   private void bindEventsToImplementation() {
@@ -38,8 +58,19 @@ public class LengthUnitsPresenter {
     view.setOnMilesChanged(miles -> convert(miles, MILE));
   }
 
-  private void convert(String sourceLength, Object sourceUnit) {
-    BigDecimal conversionValue = sourceLength == null ? null : new BigDecimal(sourceLength);
-    model.convert(conversionValue, sourceUnit);
+  private BigDecimal toBigDecimal(String value) {
+    try {
+      return new BigDecimal(getDecimalFormat().parse(value).doubleValue());
+    } catch (ParseException e) {
+      return null;
+    }
+  }
+
+  private String formatBigDecimal(BigDecimal value) {
+    try {
+      return getDecimalFormat().format(value);
+    } catch (IllegalArgumentException e) {
+      return "";
+    }
   }
 }
