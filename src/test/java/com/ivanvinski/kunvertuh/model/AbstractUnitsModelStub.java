473653b4a17ef28e5aa531b01f7a21dc04c204bf
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-11 14:32:06 +0100

    Add AbstractUnitsModelStub

---
+++ src/test/java/com/ivanvinski/kunvertuh/model/AbstractUnitsModelStub.java
@@ -0,0 +1,10 @@
+package com.ivanvinski.kunvertuh.model;
+
+import java.math.BigDecimal;
+
+public class AbstractUnitsModelStub extends AbstractUnitsModel {
+
+  @Override
+  public void convert(BigDecimal sourceValue, Object sourceUnit) {
+  }
+}
