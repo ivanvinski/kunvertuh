2e0e32484cdc78f5a03548da66516beb1e9b9855
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-13 21:24:37 +0100

    Update ViewLoader test case
    Since Guice is used for injection, ViewLoader tests were broken because
    an extra parameter was added to the loading method. Now these tests are
    working again.

--- src/test/java/com/ivanvinski/kunvertuh/view/TestViewLoader.java
+++ src/test/java/com/ivanvinski/kunvertuh/view/TestViewLoader.java
@@ -2,6 +2,8 @@ package com.ivanvinski.kunvertuh.view;
 
 import static org.junit.Assert.assertEquals;
 
+import com.google.inject.AbstractModule;
+import com.ivanvinski.kunvertuh.module.TestParentViewPresenterModule;
 import java.net.URL;
 import org.junit.Test;
 
@@ -11,28 +13,38 @@ public class TestViewLoader {
       .getResource("/view/test-parent.fxml");
   private static final URL TEST_NON_PARENT_LOCATION = TestViewLoader.class
       .getResource("/view/test-non-parent.fxml");
+  private static final AbstractModule moduleStub = new AbstractModule() {
+    @Override
+    protected void configure() {
+    }
+  };
 
   private ViewLoader viewLoader = new ViewLoader();
 
   @Test(expected = IllegalStateException.class)
   public void nullUrlLoadingThrowsException() {
-    viewLoader.load(null);
+    viewLoader.load(null, moduleStub);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void nullModuleLoadingThrowsException() {
+    viewLoader.load(TEST_PARENT_LOCATION, null);
   }
 
   @Test(expected = ClassCastException.class)
   public void nonParentViewClassLoadingThrowsException() {
-    viewLoader.load(TEST_NON_PARENT_LOCATION);
+    viewLoader.load(TEST_NON_PARENT_LOCATION, moduleStub);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void duplicateViewLoadingThrowsException() {
-    viewLoader.load(TEST_PARENT_LOCATION);
-    viewLoader.load(TEST_PARENT_LOCATION);
+    viewLoader.load(TEST_PARENT_LOCATION, new TestParentViewPresenterModule());
+    viewLoader.load(TEST_PARENT_LOCATION, new TestParentViewPresenterModule());
   }
 
   @Test
   public void validViewLoadingRegistersView() {
-    viewLoader.load(TEST_PARENT_LOCATION);
+    viewLoader.load(TEST_PARENT_LOCATION, new TestParentViewPresenterModule());
     assertEquals(TestParentView.class,
         viewLoader.getLoadedViews().get(TestParentView.class).getClass());
   }
