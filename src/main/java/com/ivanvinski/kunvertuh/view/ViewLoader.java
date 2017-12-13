9d58ecad67c43355d05ea2e2aac6ce5e526ba634
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-13 15:58:28 +0100

    Refactor ViewLoader

--- src/main/java/com/ivanvinski/kunvertuh/view/ViewLoader.java
+++ src/main/java/com/ivanvinski/kunvertuh/view/ViewLoader.java
@@ -1,9 +1,11 @@
 package com.ivanvinski.kunvertuh.view;
 
+import com.google.inject.AbstractModule;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 import javafx.fxml.FXMLLoader;
@@ -13,12 +15,15 @@ public class ViewLoader {
 
   private Map<Class<? extends Parent>, Parent> loadedViews = new HashMap<>();
 
-  public void load(URL fxmlFile) {
+  public void load(URL fxmlFile, AbstractModule module) {
     FXMLLoader loader = new FXMLLoader(fxmlFile);
+    ViewAssembler assembler = new ViewAssembler(module);
+    loader.setControllerFactory(assembler);
     try {
       loader.load();
       // Controller is actually view because fx:controller points to view class
       registerView(fxmlFile, loader.getController());
+      assembler.getPresenter().initialize();
     } catch (IOException e) {
       throw new UncheckedIOException("Can't load view: " + fxmlFile, e);
     }
