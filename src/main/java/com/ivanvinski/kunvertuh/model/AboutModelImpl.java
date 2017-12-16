25dec6fa3c1677615257c7286cdec07e9063efa1
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-16 16:03:44 +0100

    Refactor AboutModel implementation

--- src/main/java/com/ivanvinski/kunvertuh/model/AboutModelImpl.java
+++ src/main/java/com/ivanvinski/kunvertuh/model/AboutModelImpl.java
@@ -3,15 +3,16 @@ package com.ivanvinski.kunvertuh.model;
 import java.util.Objects;
 import javafx.application.HostServices;
 
-public class AboutModel {
+public class AboutModelImpl implements AboutModel {
 
   private HostServices services;
 
-  public AboutModel(HostServices services) {
+  public AboutModelImpl(HostServices services) {
     this.services = Objects.requireNonNull(services, "Host services can't be null");
   }
 
-  public void openInBrowser(String webPageURI) {
-    services.showDocument(webPageURI);
+  @Override
+  public void openInBrowser(String uri) {
+    services.showDocument(uri);
   }
 }
--- src/main/java/com/ivanvinski/kunvertuh/module/AboutPresenterModule.java
+++ src/main/java/com/ivanvinski/kunvertuh/module/AboutPresenterModule.java
@@ -2,21 +2,22 @@ package com.ivanvinski.kunvertuh.module;
 
 import com.google.inject.AbstractModule;
 import com.ivanvinski.kunvertuh.model.AboutModel;
+import com.ivanvinski.kunvertuh.model.AboutModelImpl;
 import com.ivanvinski.kunvertuh.view.AboutView;
 import com.ivanvinski.kunvertuh.view.javafx.AboutViewImpl;
 import javafx.application.HostServices;
 
 public class AboutPresenterModule extends AbstractModule {
 
-  private HostServices hostServices;
+  private AboutModel model;
 
-  public AboutPresenterModule(HostServices hostServices) {
-    this.hostServices = hostServices;
+  public AboutPresenterModule(AboutModel model) {
+    this.model = model;
   }
 
   @Override
   protected void configure() {
     bind(AboutView.class).to(AboutViewImpl.class);
-    bind(AboutModel.class).toInstance(new AboutModel(hostServices));
+    bind(AboutModel.class).toInstance(model);
   }
 }
