6c71924768744563b2d7e8e08cf14186fa8e636c
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-18 18:43:22 +0100

    Update IMPERIAL_DELTA test constant

--- src/test/java/com/ivanvinski/kunvertuh/TestConstants.java
+++ src/test/java/com/ivanvinski/kunvertuh/TestConstants.java
@@ -3,7 +3,7 @@ package com.ivanvinski.kunvertuh;
 public final class TestConstants {
 
   public static final double METRIC_DELTA = 0d;
-  public static final double IMPERIAL_DELTA = .1d;
+  public static final double IMPERIAL_DELTA = .001d;
 
   private TestConstants() {
   }
