953da27a19f17f92db9901c407ac6ef0bec41473
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-21 06:26:10 +0100

    Change unit conversion system
    Originally, I created a triad method for unit conversion. The parameters
    were source value, source unit, and output unit. However, I believe that
    is not a clean approach.
    
    The current unit conversion looks like this:
    
        unitConverter.convert(100, METER, KILOMETER);
    
    If you don't know the order of parameters, you can only guess what this
    does. Because of this, I decided to subclass unit converter class for
    each category, so the current unit conversion also looks like this:
    
       lengthUnitConverter.toKilometers(100, METER);
    
    However, this approach also sucks because when units are modified, this
    class needs to be modified as well. I want to write code that applies
    changes as "automatically" as possible. That's why I implemented the new
    unit conversion system that looks like this:
    
       unitConverter.convert(100, METER).to(KILOMETER);
    
    As you can see, it is clear what is being converted. Perhaps the con of
    this approach is using inner class, but I don't really care since the
    pros outweight the cons. Although, I must admit I don't know if inner
    classes are code smell or not, but they generally look ugly to me.

---
+++ src/main/java/com/ivanvinski/kunvertuh/unit/UnitConverter.java
@@ -0,0 +1,50 @@
+/*
+ * Kunvertuh
+ * Copyright (C) 2017  Ivan Vinski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.ivanvinski.kunvertuh.unit;
+
+import java.util.Objects;
+
+public final class UnitConverter<U extends MeasurementUnit> {
+
+  public UnitConversion<U> convert(Double value, U unit) {
+    return new UnitConversion<>(value, unit);
+  }
+
+  public static final class UnitConversion<U extends MeasurementUnit> {
+
+    private Double sourceValue;
+    private U sourceUnit;
+
+    private UnitConversion(Double sourceValue, U sourceUnit) {
+      this.sourceValue = sourceValue;
+      this.sourceUnit = sourceUnit;
+    }
+
+    public Double to(U targetUnit) {
+      Objects.requireNonNull(sourceUnit, "Source unit can't be null");
+      Objects.requireNonNull(targetUnit, "Target unit can't be null");
+      if (sourceValue == null) {
+        return null;
+      }
+      double baseUnitValue = sourceValue * sourceUnit.getBaseUnitFactor();
+      return baseUnitValue / targetUnit.getBaseUnitFactor();
+    }
+  }
+}
