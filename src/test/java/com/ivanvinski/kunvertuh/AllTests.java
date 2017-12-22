f577cb4bbe64a37ac9a6ba5596274ec786ec99cb
ivanvinski <ivan.vinski.dev@gmail.com>
2017-12-22 21:37:27 +0100

    Remove all tests
    I refused to write tests until I settled for software architecture that
    I'd consider clean. Because of that, these tests became outdated so now
    it's time to rewrite them.
    
    I used to update tests when I changed the architecture, but it became
    useless since I'd always have to add, remove or change tests when I'd
    change the architecture.
    
    I've developed an architecture I like, so now it's time to create new
    tests.

--- src/test/java/com/ivanvinski/kunvertuh/AllTests.java
+++ src/test/java/com/ivanvinski/kunvertuh/AllTests.java
@@ -19,26 +19,6 @@
 
 package com.ivanvinski.kunvertuh;
 
-import com.ivanvinski.kunvertuh.model.LengthUnitsModelTest;
-import com.ivanvinski.kunvertuh.model.MainModelTest;
-import com.ivanvinski.kunvertuh.model.MassUnitModelTest;
-import com.ivanvinski.kunvertuh.model.VolumeUnitsModelTest;
-import com.ivanvinski.kunvertuh.presenter.AboutPresenterTest;
-import com.ivanvinski.kunvertuh.presenter.LengthUnitsPresenterTest;
-import com.ivanvinski.kunvertuh.presenter.MainPresenterTest;
-import com.ivanvinski.kunvertuh.presenter.MassUnitsPresenterTest;
-import com.ivanvinski.kunvertuh.presenter.VolumeUnitsPresenterTest;
-import com.ivanvinski.kunvertuh.unit.LengthUnitTest;
-import com.ivanvinski.kunvertuh.unit.MassUnitTest;
-import com.ivanvinski.kunvertuh.unit.VolumeUnitTest;
-import com.ivanvinski.kunvertuh.unit.converter.LengthUnitConverterTest;
-import com.ivanvinski.kunvertuh.unit.converter.MassUnitConverterTest;
-import com.ivanvinski.kunvertuh.unit.converter.MeasurementUnitConverterTest;
-import com.ivanvinski.kunvertuh.unit.converter.VolumeUnitConverterTest;
-import com.ivanvinski.kunvertuh.util.DoubleStringConverterTest;
-import com.ivanvinski.kunvertuh.view.ViewCatalogTest;
-import com.ivanvinski.kunvertuh.view.javafx.ViewAssemblerTest;
-import com.ivanvinski.kunvertuh.view.javafx.ViewLoaderImplTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -46,26 +26,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    LengthUnitTest.class,
-    MassUnitTest.class,
-    VolumeUnitTest.class,
-    MeasurementUnitConverterTest.class,
-    LengthUnitConverterTest.class,
-    MassUnitConverterTest.class,
-    VolumeUnitConverterTest.class,
-    LengthUnitsModelTest.class,
-    LengthUnitsPresenterTest.class,
-    MassUnitModelTest.class,
-    MassUnitsPresenterTest.class,
-    VolumeUnitsModelTest.class,
-    VolumeUnitsPresenterTest.class,
-    MainModelTest.class,
-    MainPresenterTest.class,
-    AboutPresenterTest.class,
-    DoubleStringConverterTest.class,
-    ViewAssemblerTest.class,
-    ViewCatalogTest.class,
-    ViewLoaderImplTest.class,
 })
 public class AllTests {
 
